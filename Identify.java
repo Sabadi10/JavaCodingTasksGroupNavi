751c8dfa1c7634e7c8daa7bf35dd63e8f2a5fa5e
AnnaChupina <artareaanna@gmail.com>
2023-05-23 19:34:48 -0400

    create class with method identify

---
+++ Identify.java
@@ -0,0 +1,9 @@
+public class Identify {
+    public static void identify (int num){
+        if(num%2 == 0){
+            System.out.println(num + " is even");
+        } else if(num%2==1){
+            System.out.println(num + " is odd");
+        }
+    }
+}
